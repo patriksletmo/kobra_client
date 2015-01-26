283de7d066c48d3e3f9dcd5afe710d5ecef9b55a
Patrik Sletmo <patrik.sletmo@gmail.com>
2015-01-26 18:00:26 +0100

    Implemented request timeout support

--- lib/kobra/client.rb
+++ lib/kobra/client.rb
@@ -9,6 +9,7 @@ module Kobra
     class NotFound < RuntimeError; end
     class AuthError < RuntimeError; end
     class ServerError < RuntimeError; end
+    class TimeoutError < RuntimeError; end
 
     # Kobra::Client.new(:domain => "kobra.ks.liu.se", :username => "john", :api_key => "a3h93hu393")
     def initialize(settings = {})
@@ -18,19 +19,19 @@ module Kobra
 
     # get_student(:liu_id => 'johec890')
     # Parameters: liu_id, email, personal_number, rfid_number, barcode_number
-    def get_student(parameters)
-      json_post('students/api.json', parameters)
+    def get_student(parameters, timeout=nil)
+      json_post('students/api.json', parameters, timeout)
     end
 
     private
 
     # Posts to path with parameters, returns JSON with symbols
-    def json_post(path, parameters = {})
+    def json_post(path, parameters = {}, timeout=nil)
       # Make sure parameters is a Hash
       parameters = {}.merge(parameters)
       url = @base_url + path
 
-      response = RestClient.post(url, parameters)
+      response = RestClient::Request.execute(method: :post, url: url, payload: parameters, timeout: timeout)
 
       JSON.parse(response.body, :symbolize_names => true)
     rescue JSON::ParserError
@@ -41,6 +42,8 @@ module Kobra
       raise AuthError, "Authentification failed"
     rescue RestClient::InternalServerError
       raise ServerError, "Server error"
+    rescue RestClient::RequestTimeout
+      raise TimeoutError, "Server didn't respond"
     end
   end
 end
